afcf827c3b1a812846abf3f8abdbcb45c22ce4ad
Michaël J TOUATI <touati.mickael@gmail.com>
2021-09-28 13:55:40 +0200

    add tests

---
+++ sources/input-chk.f90
@@ -0,0 +1,17 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                                   !!
+!!          1D-1V ElectroStatic Vlasov-Maxwell (ESV) code            !!
+!!                                                                   !!
+!!  Written by Dr Michaël J TOUATI - CLPU - 2020 - mtouati@clpu.es   !!
+!!                                                                   !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+program CHECK_INPUT_DECK
+!
+use acuracy
+use constants
+use input
+!
+call read_init_parameters()
+!
+end program CHECK_INPUT_DECK
