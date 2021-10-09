3c0cc750fd1e3fd1d05812d6bf4ce9f83e38209c
Michael Touati <14829604+michaeltouati@users.noreply.github.com>
2021-10-10 01:11:33 +0200

    Update physics.f90

--- sources/physics.f90
+++ sources/physics.f90
@@ -36,21 +36,21 @@ function omega_pe(n0)
   implicit none
   real(PR), intent(in) :: n0       ! (/cm^3)
   real(PR)             :: omega_pe ! (/s)
-  omega_pe = sqrt(4 * pi * n0 * (e**2._PR) / me)
+  omega_pe = sqrt(4._PR * pi * n0 * (e**2._PR) / me)
 end function omega_pe
   
 function v_T(Te)
   implicit none
   real(PR), intent(in) :: Te  ! Te (K)
   real(PR)             :: v_T ! v_T (cm/s)
-  v_T = (3._PR* kb * Te / me )**0.5_PR
+  v_T = (kb * Te / me )**0.5_PR
 end function v_T
   
 function Debye(n0, Te)
   implicit none
   real(PR), intent(in) :: n0, Te ! ne (/cm^3) and T (K)
   real(PR)             :: Debye  ! Debye (cm)
-  Debye = ( kB * Te / (4 * pi * n0 * (e**2._PR)) )**(0.5_PR) 
+  Debye = ( kB * Te / (4._PR * pi * n0 * (e**2._PR)) )**(0.5_PR) 
 end function Debye
 
 end module physics
