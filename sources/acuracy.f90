72c636ccb5da9052d6950e652eac0731cdee7e48
Michael Touati <14829604+michaeltouati@users.noreply.github.com>
2021-10-10 01:07:36 +0200

    Update acuracy.f90

--- sources/acuracy.f90
+++ sources/acuracy.f90
@@ -17,7 +17,7 @@
 !! GNU General Public License for more details.                      !!
 !!                                                                   !!
 !! You should have received a copy of the GNU General Public License !!
-!! along with ESVM.  If not, see <https://www.gnu.org/licenses/>.    !!
+!! along with ESVM. If not, see <https://www.gnu.org/licenses/>.     !!
 !!                                                                   !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! Initial commit written by Michaël J TOUATI - Dec. 2015
