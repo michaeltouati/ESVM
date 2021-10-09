6690e78f616c53404b5421a60ecfb886ad3ff660
Michael Touati <14829604+michaeltouati@users.noreply.github.com>
2021-10-10 01:05:31 +0200

    Update acuracy.f90

--- sources/acuracy.f90
+++ sources/acuracy.f90
@@ -17,8 +17,7 @@
 !! GNU General Public License for more details.                      !!
 !!                                                                   !!
 !! You should have received a copy of the GNU General Public License !!
-!! along with Foobar.  If not, see <https://www.gnu.org/licenses/>.  !!
-!! Initial commit written by Dr Michaël J TOUATI - Dec. 2015         !!
+!! along with ESVM.  If not, see <https://www.gnu.org/licenses/>.    !!
 !!                                                                   !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !! Initial commit written by Michaël J TOUATI - Dec. 2015
