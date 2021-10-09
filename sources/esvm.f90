bfb5c3cac122beea2af41a7639f0ce3be0bf8aae
Michael Touati <14829604+michaeltouati@users.noreply.github.com>
2021-10-10 01:08:02 +0200

    Update esvm.f90

--- sources/esvm.f90
+++ sources/esvm.f90
@@ -2,10 +2,25 @@
 !!                                                                   !!
 !!             ElectroStatic Vlasov-Maxwell (ESVM) code              !!
 !!                                                                   !!
-!! Initial commit written by Dr Michaël J TOUATI - Dec. 2015         !!
+!! Copyright © 2015 Michaël J TOUATI                                 !!
+!!                                                                   !!
+!! This file is part of ESVM.                                        !!
+!!                                                                   !!
+!! ESVM is free software: you can redistribute it and/or modify      !!
+!! it under the terms of the GNU General Public License as published !!
+!! by the Free Software Foundation, either version 3 of the License, !!
+!! or (at your option) any later version.                            !!
+!!                                                                   !!
+!! ESVM is distributed in the hope that it will be useful,           !!
+!! but WITHOUT ANY WARRANTY; without even the implied warranty of    !!
+!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the     !!
+!! GNU General Public License for more details.                      !!
+!!                                                                   !!
+!! You should have received a copy of the GNU General Public License !!
+!! along with ESVM. If not, see <https://www.gnu.org/licenses/>.     !!
 !!                                                                   !!
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!
+!! Initial commit written by Michaël J TOUATI - Dec. 2015
 program ESVM
 !
 use acuracy
